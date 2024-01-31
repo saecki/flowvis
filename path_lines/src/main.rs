88cfc433512dd01dae51b847c6b8f473ff0e3abe
Saecki <tobiasschmitz2001@gmail.com>
2024-01-31 20:14:57 +0100

    path_lines: vertex and index buffer

--- path_lines/src/main.rs
+++ path_lines/src/main.rs
@@ -1,5 +1,7 @@
+use std::borrow::Cow;
 use std::path::Path;
 
+use wgpu::util::DeviceExt;
 use winit::dpi::PhysicalSize;
 use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -46,6 +48,30 @@ impl FlowField {
     }
 }
 
+#[rustfmt::skip]
+const PENTAGON_VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+#[rustfmt::skip]
+const PENTAGON_INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
+#[rustfmt::skip]
+const SQUARE_VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.5, 0.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.5, 0.0] },
+    Vertex { position: [-0.5, 0.5, 0.0], color: [0.0, 0.5, 0.0] },
+    Vertex { position: [0.5, 0.5, 0.0], color: [0.0, 0.0, 0.5] },
+];
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 struct Vec2 {
     x: f32,
@@ -53,15 +79,72 @@ struct Vec2 {
 }
 
 struct State {
+    window: Window,
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
-    window: Window,
+
+    current_pipline: CurrentPipeline,
+    pipeline1: Pipeline1,
+    pipeline2: Pipeline2,
 
     color: wgpu::Color,
     flow_field: FlowField,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CurrentPipeline {
+    P1 = 1,
+    P2 = 2,
+}
+
+struct Pipeline1 {
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
+}
+
+struct Pipeline2 {
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    /// [
+    ///     wgpu::VertexAttribute {
+    ///         offset: 0,
+    ///         shader_location: 0,
+    ///         format: wgpu::VertexFormat::Float32x3,
+    ///     },
+    ///     wgpu::VertexAttribute {
+    ///         offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+    ///         shader_location: 1,
+    ///         format: wgpu::VertexFormat::Float32x3,
+    ///     },
+    /// ],
+    const ATTRIBUTES: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![
+        0 => Float32x3,
+        1 => Float32x3,
+    ];
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBUTES,
+        }
+    }
 }
 
 impl State {
@@ -117,6 +200,139 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        let pipeline1 = {
+            let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("shader"),
+                source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+            });
+            let render_pipeline_layout =
+                device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+            let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline 1"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[Vertex::desc()],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+            });
+
+            let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer 1"),
+                contents: bytemuck::cast_slice(PENTAGON_VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+
+            let num_indices = PENTAGON_INDICES.len() as u32;
+            let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer 1"),
+                contents: bytemuck::cast_slice(PENTAGON_INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            });
+
+            Pipeline1 {
+                render_pipeline,
+                vertex_buffer,
+                index_buffer,
+                num_indices,
+            }
+        };
+
+        let pipeline2 = {
+            let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+                label: Some("shader"),
+                source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+            });
+            let render_pipeline_layout =
+                device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+            let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline 2"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[Vertex::desc()],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleStrip,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+            });
+
+            let num_vertices = SQUARE_VERTICES.len() as u32;
+            let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer 2"),
+                contents: bytemuck::cast_slice(SQUARE_VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+
+            Pipeline2 {
+                render_pipeline,
+                vertex_buffer,
+                num_vertices,
+            }
+        };
+
         Self {
             window,
             surface,
@@ -124,6 +340,11 @@ impl State {
             queue,
             config,
             size,
+
+            current_pipline: CurrentPipeline::P1,
+            pipeline1,
+            pipeline2,
+
             color: wgpu::Color {
                 r: 0.1,
                 g: 0.2,
@@ -149,6 +370,23 @@ impl State {
 
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        scancode,
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                use CurrentPipeline::*;
+                self.current_pipline = match self.current_pipline {
+                    P1 => P2,
+                    P2 => P1,
+                };
+                true
+            }
             WindowEvent::CursorMoved { position, .. } => {
                 self.color.r = position.x / self.size.width as f64;
                 self.color.b = position.y / self.size.height as f64;
@@ -173,20 +411,39 @@ impl State {
                 label: Some("Render Encoder"),
             });
 
-        _ = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: Some("Render Pass"),
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(self.color),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            occlusion_query_set: None,
-            timestamp_writes: None,
-        });
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.color),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+
+            match self.current_pipline {
+                CurrentPipeline::P1 => {
+                    let p1 = &self.pipeline1;
+                    render_pass.set_pipeline(&p1.render_pipeline);
+                    render_pass.set_vertex_buffer(0, p1.vertex_buffer.slice(..));
+                    render_pass
+                        .set_index_buffer(p1.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                    render_pass.draw_indexed(0..p1.num_indices, 0, 0..1);
+                }
+                CurrentPipeline::P2 => {
+                    let p2 = &self.pipeline2;
+                    render_pass.set_pipeline(&p2.render_pipeline);
+                    render_pass.set_vertex_buffer(0, p2.vertex_buffer.slice(..));
+                    render_pass.draw(0..p2.num_vertices, 0..1);
+                }
+            }
+        }
 
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
